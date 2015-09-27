11e15ea8e2c753920cc801c885d8ecd03148fac9
zhikhe <zhikhe@gmail.com>
2015-09-27 01:37:30 -0400

    added button/fixed img size

--- app/src/main/java/com/thirteenglasses/tourbuzzy/MainActivity.java
+++ app/src/main/java/com/thirteenglasses/tourbuzzy/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        IMG = (ImageView)findViewById(R.id.fab);
+        IMG = (ImageView)findViewById(R.id.IMG);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
