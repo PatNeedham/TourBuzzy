e82e9d81763bb44977ea14ea1b2a7a2e39236dd7
zhikhe <zhikhe@gmail.com>
2015-09-26 23:31:00 -0400

    added a not that func cam

--- app/src/main/java/com/thirteenglasses/tourbuzzy/MainActivity.java
+++ app/src/main/java/com/thirteenglasses/tourbuzzy/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thirteenglasses.tourbuzzy;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +11,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int REQUEST_CODE = 1;
+    ImageView IMG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        IMG = (ImageView)findViewById(R.id.fab);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View arg0) {
+                 /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show(); */
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(i.resolveActivity(getPackageManager())!=null)
+                {
+                    startActivityForResult(i, REQUEST_CODE);
+                }
             }
         });
     }
@@ -33,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onActivityResult(int requestcode, int resultcode, Intent data){
+        if(requestcode==REQUEST_CODE)
+        {
+            if(resultcode==RESULT_OK)
+            {
+                Bundle bundle = new Bundle();
+                bundle = data.getExtras();
+                Bitmap BMP;
+                BMP = (Bitmap)bundle.get("data");
+                IMG.setImageBitmap(BMP);
+            }
+        }
     }
 
     @Override
